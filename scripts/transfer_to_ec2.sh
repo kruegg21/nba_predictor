b2dc01a29df47a2bf4f925345c04dea70f3c96b7
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-01 21:25:06 -0700

    changed script

--- scripts/transfer_to_ec2.sh
+++ scripts/transfer_to_ec2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 IP=35-161-120-71
+IP2=172-31-20-190
 PREFIX=ubuntu@ec2-
 SUFFIX=.us-west-2.compute.amazonaws.com
 INSTANCE=ubuntu@ec2-35-160-200-4.us-west-2.compute.amazonaws.com
 DATA=~/Desktop/nba_predictor/data/merged_data.csv
 KEY=~/Downloads/EC2_instance1.pem
-scp -Ci $KEY $DATA $PREFIX$IP$SUFFIX:~/Desktop/nba_predictor
+scp -Ci $KEY $DATA $PREFIX$IP2$SUFFIX:~/Desktop/nba_predictor
