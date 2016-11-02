6dbfddbd60d509f9125933764cff6348aac64ae5
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-01 21:31:02 -0700

    changed scripts

--- scripts/connect_to_instance.sh
+++ scripts/connect_to_instance.sh
@@ -1,6 +1,5 @@
 KEY=~/Downloads/EC2_instance1.pem
 PREFIX=ubuntu@ec2-
 SUFFIX=.us-west-2.compute.amazonaws.com
-IP=35-161-120-71
-IP2=35-160-76-32
-ssh -i $KEY $PREFIX$IP2$SUFFIX
+IP=35-161-148-176
+ssh -i $KEY $PREFIX$IP$SUFFIX
